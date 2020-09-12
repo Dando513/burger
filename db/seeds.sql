6bea9ccb50dc9a638b0bff46e1fa0b4d092693fb
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-09-11 21:50:17 -0400

    ready to grade

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Good Burger');
+INSERT INTO burger (name) VALUES ('Fat Burger');
+INSERT INTO burger (name, eaten) VALUES ('Royale', true);
+INSERT INTO burger (name, eaten) VALUES ('Smash Burger', true);
+INSERT INTO burger (name, eaten) VALUES ('Quarter Pounder', true);
+INSERT INTO burger (name) VALUES ('Big Mac');
